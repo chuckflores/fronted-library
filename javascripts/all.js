73a891348b6507ce984b524fe0d96e0ae1faacf9
cef <chuck.e.flores@gmail.com>
2016-11-08 16:24:47 -0500

    Automated commit at 2016-11-08 21:24:47 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,12 +1,1 @@
-// This is where it all goes :)
-$( document ).ready(function() {
-  $('#menu').click(function() {
-    $('.nav-bar').toggleClass('expand');
-    $('.nav-items li').toggleClass('show');
-  })
-  $('.expand-details').click(function() {
-    $(this).prev()
-      .find('.details')
-      .slideToggle();
-  })
-});
+$(document).ready(function(){$("#menu").click(function(){$(".nav-bar").toggleClass("expand"),$(".nav-items li").toggleClass("show")}),$(".expand-details").click(function(){$(this).prev().find(".details").slideToggle()})});
